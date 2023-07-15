b51395a3061208d6272c2a2b72cac20aade64874
Mariusz Lepkowski <mariusz.lepkowski23@gmail.com>
2023-07-15 15:05:07 +0200

    Create new repository

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+import requests
+
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template("index.html", posts=posts)
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+
+@app.route('/post/<int:index>')
+def show_post(index):
+    requested_post = None
+    for post in posts:
+        if post["id"] == index:
+            requested_post = post
+    return render_template("post.html", post=requested_post)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
